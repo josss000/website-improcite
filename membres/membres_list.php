80939f2ed3f84387d52226b386568c3e509503ea
jossssss@gmail.com <jossssss@gmail.com>
2016-07-21 20:00:59 +0200

    Essai sur cartouche membre

--- membres/membres_list.php
+++ membres/membres_list.php
@@ -6,71 +6,162 @@ require_once ("tete.php");
 
 <div class="row">
     <div class="col-md-4">
-		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
-		    <div class="content">
-		    	Contenu, blablabla
-		    </div>
-		</div>
+		 <div class="widget">
+		    	<div class="row">
+		    		<div class="avatar-header">
+						<img class="avatar border-white" src="../img/profil/arnaud.jpg" alt="..."/>					
+					</div>
+					<div class="avatar-content">
+						<h4>Arnaud</h4>
+                        <hr>
+                        <div class="content">
+                                   		    	Contenu, blablabla
+                        </div>
+                        <hr>
+                        footer
+			    	</div>
+		    	</div>
+		 </div>
 	</div>
-    <div class="col-md-4">
-		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
-		    <div class="content">
-		    	Contenu, blablabla
-		    </div>
-		</div>	
-	</div>		
-    <div class="col-md-4">
-		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
-		    <div class="content">
-		    	Contenu, blablabla
-		    </div>
-		</div>
+	<div class="col-md-4">
+		 <div class="widget">
+			ezr
+		 </div>
+	</div>
+	<div class="col-md-4">
+		 <div class="widget">
+		rez
+		 </div>
 	</div>
 </div>
 
+<?php
+/*
 <div class="row">
     <div class="col-md-4">
-		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
-		    <div class="content">
-		    	Contenu, blablabla
-		    </div>
+		 <div class="card widget">
+		 	<div class="row">
+			 	<div class="col-md-4">
+			       	<div class="image">
+	                <img src="../img/profil_background_widget.jpg" alt="..."/>
+	            	</div>
+				</div>
+			 	<div class="col-md-8">
+			 		 <div class="content">
+					 	 <div class="row">
+					    	Contenu, blablabla<br>
+					    	blablbla<br>
+					    	blbla
+						</div>
+					</div>
+					<div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-user"></i> 7
+                                        <i class="ti-panel"></i> 8
+                                        <i class="ti-reload"></i> 9
+                                        modifier
+                                    </div>
+     	           </div>
+		 		</div>
+            </div>
 		</div>
 	</div>
     <div class="col-md-4">
 		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
-		    <div class="content">
-		    	Contenu, blablabla
+			<div class="row">
+		 		<div class="col-md-4">
+			 		<div class="content header">
+			             <img class="avatar border-white" src="../img/profil/josselin.jpg" alt="..."/>
+			         </div>
+			    </div>		
+		    	<div class="col-md-8">	
+				    <div class="content">
+					<div class="row">
+				    	Contenu, blablabla<br>
+				    	blablbla<br>
+				    	blbla
+					    </div>
+					</div>
+					<div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-user"></i> 7
+                                        <i class="ti-panel"></i> 8
+                                        <i class="ti-reload"></i> 9
+                                        modifier
+                                    </div>
+     	           </div>
+     	       </div>
 		    </div>
 		</div>	
 	</div>		
     <div class="col-md-4">
 		 <div class="card">
-		    <div class="header">
-		        <h4 class="title">Prénom</h4>
-		    </div>
 		    <div class="content">
-		    	Contenu, blablabla
+            <img class="avatar border-white" src="../img/profil/arnaud.jpg" alt="..."/>
+                                  		    	Contenu, blablabla
 		    </div>
 		</div>
 	</div>
 </div>
 
-<?
+    <div class="col-md-4">
+		 <div class="card widget">
+		 	<div class="row">
+			 	<div class="col-md-4">
+			 		 <div class="content">
+			       	a
+			       </div>
+				</div>
+			 	<div class="col-md-8">
+			 		 <div class="content">
+					 	 <div class="row">
+					    	Contenu, blablabla<br>
+					    	blablbla<br>
+					    	blbla
+						</div>
+					</div>
+					<div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-user"></i> 7
+                                        <i class="ti-panel"></i> 8
+                                        <i class="ti-reload"></i> 9
+                                        modifier
+                                    </div>
+     	           </div>
+		 		</div>
+            </div>
+		</div>
+	</div>
+    <div class="col-md-4">
+		 <div class="card widget">
+		    <div class="content">
+				<div class="row">
+				    <div class="col-md-5">
+			 		<div class="header">
+			             <img class="avatar border-white" src="../img/profil/josselin.jpg" alt="..."/>
+			         </div>
+			    	</div>
+					<div class="col-md-7">
+					Contenu, blablabla
+				    </div>
+				</div>
+			</div>
+		</div>	
+	</div>		
+    <div class="col-md-4">
+		 <div class="card">
+		    <div class="content">
+            <img class="avatar border-white" src="../img/profil/arnaud.jpg" alt="..."/>
+                                  		    	Contenu, blablabla
+		    </div>
+		</div>
+	</div>
+</div>
+*/
+
 include_once ("pied.php");
 
 ?>
